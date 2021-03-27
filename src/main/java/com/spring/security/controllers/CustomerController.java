0b18109e9784cd7da6e9023f1fa477e894af70a0
helen <hhma@alumni.cpp.edu>
2021-03-26 21:04:28 -0400

    add customer controller

--- src/main/java/com/spring/security/controllers/CustomerController.java
+++ src/main/java/com/spring/security/controllers/CustomerController.java
@@ -6,8 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,5 +35,11 @@ public class CustomerController {
         List<Customer> customers = customerService.getAllCustomers();
         log.info("Customers -->" + customers);
         return new ResponseEntity(customers, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity createCustomer(@RequestBody Customer customer){
+        customerService.saveCustomer(customer);
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }
